1cef2ce31f6fce6ab635308179e5b188ae94a327
kornadian <kornadian@users.noreply.github.com>
2020-10-08 22:12:21 -0600

    Update service-registry.sh

--- service-registry.sh
+++ service-registry.sh
@@ -5,7 +5,7 @@ sudo mv cloudflare-ddns-from-linux.sh /usr/bin/$SERVICE_NAME.sh
 sudo chmod +x /usr/bin/$SERVICE_NAME.sh
 
 
-cat <<EOF > /lib/systemd/system/$SERVICE_NAME.service
+cat <<EOF > $SERVICE_NAME.service
 #!/bin/bash
 
 [Unit]
@@ -20,7 +20,28 @@ WantedBy=multi-user.target
 
 EOF
 
-sudo cp /lib/systemd/system/$SERVICE_NAME.service /etc/systemd/system/$SERVICE_NAME.service
+cat <<EOF > $SERVICE_NAME-timer.timer
+[Unit]
+Description=Cf DDNS DNS Update Timer for $SERVICE_NAME service
+
+[Timer]
+OnBootSec=1min
+OnUnitActiveSec=1min
+Unit=$SERVICE_NAME.service
+
+[Install]
+WantedBy=basic.target
+EOF
+
+
+sudo cp $SERVICE_NAME-timer.timer /etc/systemd/system/$SERVICE_NAME-timer.timer
+sudo chmod 644 /etc/systemd/system/$SERVICE_NAME-timer.timer
+
+
+sudo cp $SERVICE_NAME.service /etc/systemd/system/$SERVICE_NAME.service
 sudo chmod 644 /etc/systemd/system/$SERVICE_NAME.service
 
 sudo systemctl reload
+sudo systemctl enable $SERVICE_NAME-timer.timer
+sudo systemctl start $SERVICE_NAME-timer.timer
+
