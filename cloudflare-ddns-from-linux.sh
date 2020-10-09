6a9d608c3bf55ad14fad6cc0670637d8372817fd
kornadian <kornadian@users.noreply.github.com>
2020-10-09 00:24:58 -0600

    Update cloudflare-ddns-from-linux.sh

--- cloudflare-ddns-from-linux.sh
+++ cloudflare-ddns-from-linux.sh
@@ -8,7 +8,7 @@ ACOUNT_ID=33773cc6afe3b65d27fb79812c9bdb6a
 AUTH_EMAIL=allen.oh815@gmail.com
 AUTH_KEY=15c0a69788515eccc9da957608e2c9955d726
 
-TARGET_DOMAIN=ansible.aws.kornadian.com
+TARGET_DOMAIN=jenkins.aws.kornadian.com
 #####################################################
 # PRE-QUALIFICATION
 # You have to install these packages :
@@ -36,54 +36,47 @@ command -v dig &> /dev/null && {
 	exit 1
 }
 
-if($CUR_IP != $_IP)then
 
-	#========= TO FIND A DNS RECORD ID
-	#echo $DNS_ZONE
+#========= TO FIND A DNS RECORD ID
+#echo $DNS_ZONE
 
-	curl "https://api.cloudflare.com/client/v4/zones/$DNS_ZONE/dns_records?type=A&name=$TARGET_DOMAIN&content=$CUR_IP&proxied=false&page=1&per_page=20&order=type&direction=desc&match=all" \
-		 --silent \
-		 -X GET \
-		 -H "Content-Type: application/json" \
-		 -H "X-Auth-Email: $AUTH_EMAIL" \
-		 -H "X-Auth-Key: $AUTH_KEY" | jq '.' > temp-record-result
-
-
-
-
-	if($(jq '.success' temp-record-result -r) == true) then
-		RECORD_ID=$(jq '.result[0].id' temp-record-result -r)
-		echo "$(date) : DNZ_RECORD_ID finder  | $TARGET_DOMAIN  | Success DNS_RECORD_ID=$RECORD_ID   " >> /tmp/cloudflare-ddns-log.log
-
-	else	
-		echo "$(date) : DNZ_RECORD_ID finder  |  $(jq '.success' temp-record-result -r) \t $(jq '.error[0]' temp-record-result -r) \t $(jq '.messages[0]' temp-record-result -r)" >> /tmp/cloudflare-ddns-log.log
-
-	fi
+curl "https://api.cloudflare.com/client/v4/zones/$DNS_ZONE/dns_records?type=A&name=$TARGET_DOMAIN&content=$CUR_IP&proxied=false&page=1&per_page=20&order=type&direction=desc&match=all" \
+	 --silent \
+	 -X GET \
+	 -H "Content-Type: application/json" \
+	 -H "X-Auth-Email: $AUTH_EMAIL" \
+	 -H "X-Auth-Key: $AUTH_KEY" | jq '.' > temp-record-result
 
 
 
 
-	_UPDATE=$(cat << EOF
-	{ "type": "A",
-	  "name": "$TARGET_DOMAIN",
-	  "content": "$_IP",
-	  "ttl": 120,
-	  "proxied": false}
-	EOF
-	)
+if($(jq '.success' temp-record-result -r) == true) then
+	RECORD_ID=$(jq '.result[0].id' temp-record-result -r)
+	echo "$(date) : DNZ_RECORD_ID finder  | $TARGET_DOMAIN  | Success DNS_RECORD_ID=$RECORD_ID   " >> /tmp/cloudflare-ddns-log.log
 
-	curl "https://api.cloudflare.com/client/v4/zones/$DNS_ZONE/dns_records/$RECORD_ID" \
-		 --silent \
-		 -X PUT \
-		 -H "Content-Type: application/json" \
-		 -H "X-Auth-Email: $AUTH_EMAIL" \
-		 -H "X-Auth-Key: $AUTH_KEY" \
-		 -d "$_UPDATE" | jq '.' > temp-record-result
+else	
+	echo "$(date) : DNZ_RECORD_ID finder  |  $(jq '.success' temp-record-result -r) \t $(jq '.error[0]' temp-record-result -r) \t $(jq '.messages[0]' temp-record-result -r)" >> /tmp/cloudflare-ddns-log.log
 
-	echo "$(date) : DNZ_RECORD UPDATE  |  $(jq '.' temp-record-result -r)" >> /tmp/cloudflare-ddns-log.log
-
-
-	rm temp-record-result
-else
-echo "$(date)  Current IP : $CUR_IP     =    My Public IP :  $_IP" >> /tmp/cloudflare-ddns-log.log
 fi
+
+_UPDATE=$( cat << EOF
+{ "type": "A",
+"name": "$TARGET_DOMAIN",
+"content": "$_IP",
+"ttl": 120,
+"proxied": false}
+EOF
+)
+
+curl "https://api.cloudflare.com/client/v4/zones/$DNS_ZONE/dns_records/$RECORD_ID" \
+	 --silent \
+	 -X PUT \
+	 -H "Content-Type: application/json" \
+	 -H "X-Auth-Email: $AUTH_EMAIL" \
+	 -H "X-Auth-Key: $AUTH_KEY" \
+	 -d "$_UPDATE" | jq '.' > temp-record-result
+
+echo "$(date) : DNZ_RECORD UPDATE  |  $(jq '.' temp-record-result -r)" >> /tmp/cloudflare-ddns-log.log
+
+
+rm temp-record-result
