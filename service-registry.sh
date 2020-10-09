489772a9b3dc7f5c52c2a9569fa3fe1b9a9e7a8d
kornadian <kornadian@users.noreply.github.com>
2020-10-08 21:55:31 -0600

    test-service01

--- service-registry.sh
+++ service-registry.sh
@@ -1,22 +1,26 @@
 
 SERVICE_NAME=cloudflare-ddns-agent
 
-sudo cp cloudflare-ddns-from-linux.sh /usr/bin/$SERVICE_NAME.sh
+sudo mv cloudflare-ddns-from-linux.sh /usr/bin/$SERVICE_NAME.sh
 sudo chmod +x /usr/bin/$SERVICE_NAME.sh
 
 
-#$ cat <<EOF > /lib/systemd/system/$SERVICE_NAME.service
-##!/bin/bash
+cat <<EOF > /lib/systemd/system/$SERVICE_NAME.service
+#!/bin/bash
 
-#[Unit]
-#Description=Agent to register/update a dns record with Dynamic DNS of the Cloudflare
-#
-#[Service]
-#Type=simple
-#ExecStart=/bin/bash /usr/bin/test_service.sh
-#
-#[Install]
-#WantedBy=multi-user.target
+[Unit]
+Description=Agent to register/update a dns record with Dynamic DNS of the Cloudflare
 
-#EOF
+[Service]
+Type=simple
+ExecStart=/bin/bash /usr/bin/$SERVICE_NAME.sh
 
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+sudo cp /lib/systemd/system/$SERVICE_NAME.service /etc/systemd/system/$SERVICE_NAME.service
+sudo chmod 644 /etc/systemd/system/$SERVICE_NAME.service
+
+sudo systemctl reload
